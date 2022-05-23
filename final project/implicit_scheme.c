8b203d1b1c7db7afe02450884583c8e98166c08f
zyb-create <zyb150410@163.com>
2022-05-23 13:50:45 +0800

    debugging

--- final project/implicit_scheme.c
+++ final project/implicit_scheme.c
@@ -12,25 +12,29 @@ static char help[] = "Solves a tridiagonal linear system.\n\n";
 */
 #include <petscksp.h>
 #include <petscmath.h>
+#include <math.h>
 
 int main(int argc,char **args)
 {
-  Vec            x, b, u, ut, u0;          /* approx solution, RHS, exact solution */
+  Vec            x, b, u, ut, u0, f;          /* approx solution, RHS, exact solution */
   Mat            A;                /* linear system matrix */
   KSP            ksp;              /* linear solver context */
   PC             pc;               /* preconditioner context */
   PetscReal      norm=0.0,tol=10.*PETSC_MACHINE_EPSILON,normt=1.0;  /* norm of solution error */
   PetscErrorCode ierr;
   PetscInt       i,n = 101,col[3],its,rstart,rend,nlocal,rank;
-  PetscScalar    zero = 0.0,value[3],omega = 1.6,dx = 0.01, kappa = 1.0, dt=0.01, rho=1, c=1, h=0;
-  PetscScalar    at = kappa*dt/(rho*c*dx), bt = dt/(rho*c), ct = 2*dx*h/kappa;
+  PetscScalar    zero = 0.0,value[3],omega = 1.6,dx = 0.01, kappa = 1.0, dt=0.01, rho=1, c=1, h=0, t=0;
+  PetscScalar    at = kappa*dt/(rho*c*dx*dx), bt = dt/(rho*c), ct = 2*dx*h/kappa, left = 0, right = 1, l=2.0;
+  PetscScalar    pi = M_PI;
+  PetscInt       step = 0;
 
 //   at *= omega; bt *= omega;  // SOR method
 
   ierr = PetscInitialize(&argc,&args,(char*)0,help);if (ierr) return ierr;
   ierr = MPI_Comm_rank(PETSC_COMM_WORLD,&rank);CHKERRQ(ierr);
   ierr = PetscOptionsGetInt(NULL,NULL,"-n",&n,NULL);CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_WORLD,"n = %D\n",n);CHKERRQ(ierr);
+//   ierr = PetscPrintf(PETSC_COMM_WORLD,"n = %D\n",n);CHKERRQ(ierr);
+//   ierr = PetscPrintf(PETSC_COMM_WORLD,"tol = %f\n", tol);CHKERRQ(ierr);
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
          Compute the matrix and right-hand-side vector that define
@@ -45,14 +49,18 @@ int main(int argc,char **args)
   ierr = VecCreate(PETSC_COMM_WORLD,&x);CHKERRQ(ierr);
   ierr = VecSetSizes(x,PETSC_DECIDE,n);CHKERRQ(ierr);
   ierr = VecSetFromOptions(x);CHKERRQ(ierr);
-  ierr = VecDuplicate(x,&z);CHKERRQ(ierr);
+  ierr = VecDuplicate(x,&u);CHKERRQ(ierr);
+  ierr = VecDuplicate(x,&ut);CHKERRQ(ierr);
+  ierr = VecDuplicate(x,&u0);CHKERRQ(ierr);
+  ierr = VecDuplicate(x,&b);CHKERRQ(ierr);
+  ierr = VecDuplicate(x,&f);CHKERRQ(ierr);
 
   /* Identify the starting and ending mesh points on each
      processor for the interior part of the mesh. We let PETSc decide
-     above. */
+     above. 
+   */
   ierr = VecGetOwnershipRange(x,&rstart,&rend);CHKERRQ(ierr);
-  ierr = VecGetLocalSize(z,&nlocal);CHKERRQ(ierr);
-//   ierr = PetscPrintf(PETSC_COMM_WORLD,"nlocal = %D\n",nlocal);CHKERRQ(ierr);
+  ierr = VecGetLocalSize(x,&nlocal);CHKERRQ(ierr);
 
   /*
      Create matrix.  When using MatCreate(), the matrix format can
@@ -79,46 +87,66 @@ int main(int argc,char **args)
      For matrix assembly, each processor contributes entries for
      the part that it owns locally.
   */
-//   ierr = PetscPrintf(PETSC_COMM_WORLD,"--------------->rstart = %d,  -------> rend = %d\n",rstart,rend);CHKERRQ(ierr);
   if (!rstart) 
   {
     rstart = 1;
-    i      = 0; col[0] = 0; col[1] = 1; value[0] = 2.0; value[1] = -1.0;
+    i      = 0; col[0] = 0; col[1] = 1; value[0] = zero; value[1] = -2*at;
     ierr   = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
+    ierr   = VecSetValue(x,i,left,INSERT_VALUES);CHKERRQ(ierr);
+    ierr   = VecSetValue(f,i,zero,INSERT_VALUES);CHKERRQ(ierr);
   }
   
   if (rend == n) 
   {
     rend = n-1;
-    i    = n-1; col[0] = n-2; col[1] = n-1; value[0] = -1.0; value[1] = 2.0;
+    i    = n-1; col[0] = n-2; col[1] = n-1; value[0] = -2*at; value[1] = zero;
     ierr = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(x,i,right,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(f,i,zero,INSERT_VALUES);CHKERRQ(ierr);
   }
 
   /* Set entries corresponding to the mesh interior */
-  value[0] = -1.0; value[1] = 2.0; value[2] = -1.0;
+  value[0] = -at; value[1] = 2*at; value[2] = -at;
   for (i=rstart; i<rend; i++) 
   {
     col[0] = i-1; col[1] = i; col[2] = i+1;
     ierr   = MatSetValues(A,1,&i,3,col,value,INSERT_VALUES);CHKERRQ(ierr);
+    ierr   = VecSetValue(x,i,left + i*dx,INSERT_VALUES);CHKERRQ(ierr);
+    ierr   = VecSetValue(f,i,sin(l*pi*(left + i*dx)),INSERT_VALUES);CHKERRQ(ierr);
   }
 
   /* Assemble the matrix */
   ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
 
-  ierr = MatView(A,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+//   ierr = MatView(A,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
 
   /*
      Set exact solution; then compute right-hand-side vector.
   */
-  ierr = VecSet(z,zero);CHKERRQ(ierr);
+  ierr = VecAssemblyBegin(f);CHKERRQ(ierr);
+  ierr = VecAssemblyEnd(f);CHKERRQ(ierr);
+  ierr = VecScale(f,bt);CHKERRQ(ierr);
+  
+  ierr = VecAssemblyBegin(x);CHKERRQ(ierr);
+  ierr = VecAssemblyEnd(x);CHKERRQ(ierr);
+
+  ierr = VecCopy(x,b);CHKERRQ(ierr);
+
+  ierr = VecExp(b);CHKERRQ(ierr);
   if(rank == 0){
-     i=0;
-     ierr = VecSetValues(z,1,&i,&one,INSERT_VALUES);CHKERRQ(ierr);
+     i    = 0;
+     ierr = VecSetValue(b,i,-at*ct,INSERT_VALUES);CHKERRQ(ierr);
+     i    = n-1;
+     ierr = VecSetValue(b,i,at*ct,INSERT_VALUES);CHKERRQ(ierr);
   }
   
-  ierr = VecAssemblyBegin(z);CHKERRQ(ierr);
-  ierr = VecAssemblyEnd(z);CHKERRQ(ierr);
+  ierr = VecAssemblyBegin(b);CHKERRQ(ierr);
+  ierr = VecAssemblyEnd(b);CHKERRQ(ierr);
+
+  ierr = VecAXPY(b,1,f);CHKERRQ(ierr);
+//   ierr = VecView(b,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+//   ierr = PetscPrintf(PETSC_COMM_WORLD,"A*C = %f\n", at*ct);CHKERRQ(ierr);
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
                 Create the linear solver and set various options
@@ -145,7 +173,7 @@ int main(int argc,char **args)
   */
   ierr = KSPGetPC(ksp,&pc);CHKERRQ(ierr);
   ierr = PCSetType(pc,PCJACOBI);CHKERRQ(ierr);
-  ierr = KSPSetTolerances(ksp,1.e-7,PETSC_DEFAULT,PETSC_DEFAULT,PETSC_DEFAULT);CHKERRQ(ierr);
+  ierr = KSPSetTolerances(ksp,1.e-8,PETSC_DEFAULT,PETSC_DEFAULT,PETSC_DEFAULT);CHKERRQ(ierr);
 
   /*
     Set runtime options, e.g.,
@@ -156,17 +184,29 @@ int main(int argc,char **args)
   */
   ierr = KSPSetFromOptions(ksp);CHKERRQ(ierr);
 
-  
+  ierr = VecSet(u,zero);CHKERRQ(ierr);
   while(PetscAbsReal(norm-normt)>tol){
+     ierr = VecCopy(u,ut);CHKERRQ(ierr);
+     step = step + 1;
      normt= norm;
-     ierr = KSPSolve(ksp,z,x);CHKERRQ(ierr);
-    //  ierr = MatMult(A,z,x);CHKERRQ(ierr);
-     ierr = VecNorm(x,NORM_2,&norm);CHKERRQ(ierr);
-     ierr = VecScale(x,(PetscScalar)1.0/norm);CHKERRQ(ierr);
-     ierr = VecCopy(x,z);CHKERRQ(ierr);
+     ierr = KSPSolve(ksp,b,u);CHKERRQ(ierr);
+     ierr = VecCopy(u,u0);CHKERRQ(ierr);
+     ierr = VecAXPY(u0,-1,ut);CHKERRQ(ierr);
+     ierr = VecNorm(u0,NORM_2,&norm);CHKERRQ(ierr);
+     ierr = VecCopy(u,b);CHKERRQ(ierr);    
+     if(rank == 0){
+        i    = 0;
+        ierr = VecSetValue(b,i,-at*ct,INSERT_VALUES);CHKERRQ(ierr);
+        i    = n-1;
+        ierr = VecSetValue(b,i,at*ct,INSERT_VALUES);CHKERRQ(ierr);
+      }
+      ierr = VecAssemblyBegin(b);CHKERRQ(ierr);
+      ierr = VecAssemblyEnd(b);CHKERRQ(ierr);
+      ierr = VecAXPY(b,1,f);CHKERRQ(ierr);
+      ierr = PetscPrintf(PETSC_COMM_WORLD,"step = %d, norm = %f\n",step,norm);CHKERRQ(ierr);
    }
-  ierr = VecView(z,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_WORLD,"solution = %f\n",1.0/norm);CHKERRQ(ierr);
+  ierr = VecView(u,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+//   ierr = PetscPrintf(PETSC_COMM_WORLD,"solution = %f\n",1.0/norm);CHKERRQ(ierr);
 
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -175,7 +215,7 @@ int main(int argc,char **args)
   /*
      Solve linear system
   */
-  // ierr = KSPSolve(ksp,b,x);CHKERRQ(ierr);
+//   ierr = KSPSolve(ksp,b,x);CHKERRQ(ierr);
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
                       Check solution and clean up
@@ -194,9 +234,10 @@ int main(int argc,char **args)
      Free work space.  All PETSc objects should be destroyed when they
      are no longer needed.
   */
-  ierr = VecDestroy(&x);CHKERRQ(ierr); //ierr = VecDestroy(&u);CHKERRQ(ierr);
-  ierr = VecDestroy(&z);CHKERRQ(ierr); ierr = MatDestroy(&A);CHKERRQ(ierr);
-  ierr = KSPDestroy(&ksp);CHKERRQ(ierr);
+  ierr = VecDestroy(&x);CHKERRQ(ierr); ierr = VecDestroy(&u);CHKERRQ(ierr);
+  ierr = VecDestroy(&ut);CHKERRQ(ierr); ierr = MatDestroy(&A);CHKERRQ(ierr);
+  ierr = KSPDestroy(&ksp);CHKERRQ(ierr); ierr = VecDestroy(&b);CHKERRQ(ierr);
+  ierr = VecDestroy(&u0);CHKERRQ(ierr); ierr = VecDestroy(&f);CHKERRQ(ierr);
 
   /*
      Always call PetscFinalize() before exiting a program.  This routine
